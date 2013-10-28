0fa062783c6a48b8b00607818b2c314fe1be89ac
Rickard Lindberg <ricli85@gmail.com>
2013-10-28 11:57:22 +0100

    triangulation test

--- NodeEditor.hs
+++ NodeEditor.hs
@@ -16,10 +16,16 @@ emptyTree :: Tree
 emptyTree = Tree M.empty
 
 getNode :: Int -> Tree -> Node
-getNode id (Tree nodes) = Node 0 "" "module NodeEditor where"
+getNode id (Tree nodes) =
+    case (M.lookup id nodes) of
+        Just node -> node
+        Nothing -> Node 0 "" ""
 
 addNode :: Node -> Tree -> Tree
 addNode node (Tree nodes) = Tree (M.insert (nodeId node) node nodes)
 
 loadFile :: FilePath -> IO Tree
-loadFile path = return emptyTree
+loadFile path = return
+              $ addNode (Node 2 "" "import qualified Data.Map as M")
+              $ addNode (Node 1 "" "module NodeEditor where")
+              $ emptyTree
--- Test.hs
+++ Test.hs
@@ -6,6 +6,11 @@ main :: IO ()
 main = hspec $ do
 
     describe "Node editor" $ do
+
         it "can read nodes from file" $ do
             tree <- loadFile "NodeEditor.hs"
             (body (getNode 1 tree)) `shouldBe` "module NodeEditor where"
+
+        it "can read nodes from file" $ do
+            tree <- loadFile "NodeEditor.hs"
+            (body (getNode 2 tree)) `shouldBe` "import qualified Data.Map as M"
