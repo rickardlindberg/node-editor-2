51e59df4dc19191e90f1a45f7f22593a308a985f
Rickard Lindberg <ricli85@gmail.com>
2013-11-11 13:46:56 +0100

    Python client has a present/edit loop.

--- src/client.py
+++ src/client.py
@@ -1,17 +1,54 @@
 import json
+import subprocess
 
 import zmq
 
-context = zmq.Context()
+class Client(object):
 
-socket = context.socket(zmq.REQ)
-socket.connect("tcp://localhost:5555")
+    def __init__(self):
+        self.nodes = []
 
-nodes = []
+    def run(self):
+        self._init_zmq()
+        self._fetch_nodes_from_server()
+        while True:
+            self._print_node_list()
+            self._get_input()
 
-socket.send(json.dumps({"command":"get_top_level_nodes"}))
-nodes = json.loads(socket.recv())
+    def _init_zmq(self):
+        context = zmq.Context()
+        self.socket = context.socket(zmq.REQ)
+        self.socket.connect("tcp://localhost:5555")
 
-while True:
-    x = raw_input("Yes? ")
-    print(nodes)
+    def _fetch_nodes_from_server(self):
+        self.socket.send(json.dumps({"command":"get_top_level_nodes"}))
+        self.nodes = json.loads(self.socket.recv())
+
+    def _print_node_list(self):
+        print("Nodes:")
+        for node in self.nodes:
+            title = node["body"][:10].replace("\n", "\\n") + "..."
+            print("  (%d) %s" % (node["id"], title))
+
+    def _get_input(self):
+        id = int(raw_input("Which to edit? "))
+        node = self._get_node_with_id(id)
+        self._edit(node)
+
+    def _edit(self, node):
+        tmp_path = "/tmp/node-editor-tmp-file.txt"
+        with open(tmp_path, "w") as f:
+            f.write(node["body"])
+        subprocess.call(["vim", tmp_path])
+        print("Saving...")
+        with open(tmp_path, "r") as f:
+            node["body"] = f.read()
+
+    def _get_node_with_id(self, id):
+        for node in self.nodes:
+            if node["id"] == id:
+                return node
+        return None
+
+if __name__ == "__main__":
+    Client().run()
