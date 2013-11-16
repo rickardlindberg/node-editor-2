563e6fa9fbccccb04f06a59623e0a85dfc70cfb0
Rickard Lindberg + Z <zee+rl+z@zeespencer.com>
2013-11-16 13:19:31 -0500

    Use quickcheck when doing roundtrip conversion

--- test/ConvertingTextToTreeAndBackSpec.hs
+++ test/ConvertingTextToTreeAndBackSpec.hs
@@ -1,26 +1,34 @@
 module ConvertingTextToTreeAndBackSpec where
 
 import Test.Hspec
+import Test.Hspec.QuickCheck
+import Test.QuickCheck
 import NodeEditor.Parser
 import NodeEditor.Writer
 
+genNodeFileContent :: Gen String
+genNodeFileContent = do
+  let genBody = return "a static body"
+  let genNewlines = fmap concat $ listOf (return "\n")
+  bodies <- listOf genBody
+  newlines <- vectorOf (length bodies) genNewlines
+  let fullText = concat $ zipWith (++) bodies newlines
+  return fullText
+
 spec = do
   it "Works with a single node!" $ do
-    let src = "cd"
-    let tree = treeFromText src
-    let output = toText tree
-    output `shouldBe` src
+    isRoundtripTheSame "cd" `shouldBe` True
 
   it "Works with two nodes!" $ do
-    let src = "a\n\ncd"
-    let tree = treeFromText src
-    let output = toText tree
-    output `shouldBe` src
+    isRoundtripTheSame "a\n\ncd" `shouldBe` True
 
   it "Persists additional whitespace properly"  $ do
-    let src = "one\n\n\ntwo"
-    toText (treeFromText src) `shouldBe` src
+    isRoundtripTheSame "one\n\n\ntwo" `shouldBe` True
 
   it "Persists trailing whitespace properly" $ do
-    let src = "one\n\n\ntwo\n\n"
-    toText (treeFromText src) `shouldBe` src
+    isRoundtripTheSame "one\n\n\ntwo\n\n" `shouldBe` True
+
+  it "Works for all kinds of input" $ property $
+    forAll genNodeFileContent isRoundtripTheSame
+
+isRoundtripTheSame content = toText (treeFromText content) == content
